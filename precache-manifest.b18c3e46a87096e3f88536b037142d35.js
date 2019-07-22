1aac815cb86622957e3fc3100c5632a6ef3a4e25
alex <earthhuman41@gmail.com>
2019-07-22 13:07:21 +0300

    Updates

---
+++ precache-manifest.b18c3e46a87096e3f88536b037142d35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6078900a5dffc8e33d5e91a8b3fde06c",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "c8ba21c8b4fae21f8782",
+    "url": "/react_add-todo-form/static/css/main.093dc532.chunk.css"
+  },
+  {
+    "revision": "7699956a3fc10dfd0ce2",
+    "url": "/react_add-todo-form/static/js/2.a3957072.chunk.js"
+  },
+  {
+    "revision": "c8ba21c8b4fae21f8782",
+    "url": "/react_add-todo-form/static/js/main.36dc2c05.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
